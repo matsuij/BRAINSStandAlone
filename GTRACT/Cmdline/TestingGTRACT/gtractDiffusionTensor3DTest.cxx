09a78b82f4c34594e53c8650d521451c459e0c10
Joy Matsui <joy-matsui@uiowa.edu>
2012-07-19 17:02:25 -0500

    Bug fix and more test writing (not done)

--- GTRACT/Cmdline/TestingGTRACT/gtractDiffusionTensor3DTest.cxx
+++ GTRACT/Cmdline/TestingGTRACT/gtractDiffusionTensor3DTest.cxx
@@ -179,7 +179,7 @@ int main( int argc, char *argv[] )
     simpleSyntheticTensorPixel[ 2 ] = 0.0;
     simpleSyntheticTensorPixel[ 3 ] = 0.0007;
     simpleSyntheticTensorPixel[ 4 ] = 0.0;
-    simpleSyntheticTensorPixel[ 5 ] = 0.0005;
+    simpleSyntheticTensorPixel[ 5 ] = 0.0004;
 
     simpleSyntheticTensor->SetPixel( simpleSyntheticTensorIndex, simpleSyntheticTensorPixel );
   }
@@ -189,8 +189,6 @@ int main( int argc, char *argv[] )
     simpleSyntheticTensorIndex = simpleSyntheticTensorIt.GetIndex();
     TensorPixelType simpleSyntheticTensorPixel;
 
-    if( simpleSyntheticTensorIndex[ 0 ] == 5 && simpleSyntheticTensorIndex[ 1 ] == 5 && simpleSyntheticTensorIndex[ 2 ] == 0 )
-    {
       // Test trace and ADC
       float testTraceADC = 0.0;
       testTraceADC = static_cast< float >( simpleSyntheticTensorPixel.GetTrace() / 3.0 );
@@ -206,9 +204,9 @@ int main( int argc, char *argv[] )
 
       // Test FA
       float testFA = 0.0;
-      testFA = static_cast< float >( simpleSyntheticPixel.GetFractionalAnisotropy() );
+      testFA = static_cast< float >( simpleSyntheticTensorPixel.GetFractionalAnisotropy() );
       std::cout << "testFA: " << testFA << std::endl;
-      if( testFA != 0.725469 )
+      if( testFA != 0.317451 )
       {
         return EXIT_SUCCESS;
       }
@@ -219,9 +217,9 @@ int main( int argc, char *argv[] )
 
       // Test RA
       float testRA = 0.0;
-      testRA = static_cast< float >( simpleSyntheticPixel.GetRelativeAnisotropy() );
+      testRA = static_cast< float >( simpleSyntheticTensorPixel.GetRelativeAnisotropy() );
       std::cout << "testFA: " << testRA << std::endl;
-      if( testFA != 0.0201814 )
+      if( testFA != 0.448944 )
       {
         return EXIT_SUCCESS;
       }
@@ -232,9 +230,9 @@ int main( int argc, char *argv[] )
 
       // Test VR
       float testVR = 0.0;
-      testVR = static_cast< float >( simpleSyntheticPixel.GetVolumeRatio() );
+      testVR = static_cast< float >( simpleSyntheticTensorPixel.GetVolumeRatio() );
       std::cout << "testVR: " << testVR << std::endl;
-      if( testVR != 0.783673 )
+      if( testVR != 0.118239 )
       {
         return EXIT_SUCCESS;
       }
@@ -245,9 +243,9 @@ int main( int argc, char *argv[] )
 
       // Test AD
       float testAD = 0.0;
-      testAD = static_cast< float >( simpleSyntheticPixel.GetAxialDiffusivity() );
+      testAD = static_cast< float >( simpleSyntheticTensorPixel.GetAxialDiffusivity() );
       std::cout << "testAD: " << testAD << std::endl;
-      if( testAD != 0.000833842 )
+      if( testAD != 0.0008 )
       {
         return EXIT_SUCCESS;
       }
@@ -258,9 +256,9 @@ int main( int argc, char *argv[] )
 
       // Test RD
       float testRD = 0.0;
-      testRD = static_cast< float >( simpleSyntheticPixel.GetRadialDiffusivity() );
+      testRD = static_cast< float >( simpleSyntheticTensorPixel.GetRadialDiffusivity() );
       std::cout << "testRD: " << testRD << std::endl;
-      if( testRD != 0.000235635 )
+      if( testRD != 0.00055 )
       {
         return EXIT_SUCCESS;
       }
@@ -271,9 +269,143 @@ int main( int argc, char *argv[] )
       
       // Test LI
       float testLI = 0.0;
-      testLI = static_cast< float >( simpleSyntheticPixel.GetLatticeIndex() );
+      testLI = static_cast< float >( simpleSyntheticTensorPixel.GetLatticeIndex() );
       std::cout << "testLI: " << testLI << std::endl;
-      if( testLI != 0.625887 )
+      if( testLI != 0.015996 )
+      {
+        return EXIT_SUCCESS;
+      }
+      else
+      {
+        return EXIT_FAILURE;
+      }
+    
+   
+  
+  }
+
+  /*====================================================================
+
+    Test #3: Test scalars with synthetic tensor file that has 
+    crossterms.
+
+  ====================================================================*/
+
+  TensorImageType::RegionType compSyntheticTensorRegion( start, size );
+
+  TensorImageType::Pointer compSyntheticTensor = TensorImageType::New();
+  compSyntheticTensor->SetRegions( compSyntheticTensorRegion );
+  compSyntheticTensor->Allocate();
+
+  TensorImageType::IndexType compSyntheticTensorIndex;
+    
+  // Loop over all voxels and give them tensor values
+  ConstIteratorType compSyntheticTensorIt( compSyntheticTensor, compSyntheticTensor->GetRequestedRegion() );
+
+  for( compSyntheticTensorIt.GoToBegin(); !compSyntheticTensorIt.IsAtEnd(); ++compSyntheticTensorIt )
+  {
+    compSyntheticTensorIndex = compSyntheticTensorIt.GetIndex();
+    TensorPixelType compSyntheticTensorPixel;
+    compSyntheticTensorPixel[ 0 ] = 0.0008;
+    compSyntheticTensorPixel[ 1 ] = 0.0;
+    compSyntheticTensorPixel[ 2 ] = 0.0;
+    compSyntheticTensorPixel[ 3 ] = 0.0007;
+    compSyntheticTensorPixel[ 4 ] = 0.0;
+    compSyntheticTensorPixel[ 5 ] = 0.0004;
+
+    compSyntheticTensor->SetPixel( compSyntheticTensorIndex, compSyntheticTensorPixel );
+  }
+
+  for( compSyntheticTensorIt.GoToBegin(); !compSyntheticTensorIt.IsAtEnd(); ++compSyntheticTensorIt )
+  {
+    compSyntheticTensorIndex = compSyntheticTensorIt.GetIndex();
+    TensorPixelType compSyntheticTensorPixel;
+
+    if( compSyntheticTensorIndex[ 0 ] == 5 && compSyntheticTensorIndex[ 1 ] == 5 && compSyntheticTensorIndex[ 2 ] == 0 )
+    {
+      // Test trace and ADC
+      float testTraceADC = 0.0;
+      testTraceADC = static_cast< float >( compSyntheticTensorPixel.GetTrace() / 3.0 );
+      std::cout << "testTraceADC: " << testTraceADC << std::endl;
+      if( compSyntheticTensorPixel.GetTrace() != 0.001900000 && testTraceADC != 0.000633333 )
+      {
+        return EXIT_SUCCESS;
+      }
+      else
+      {
+        return EXIT_FAILURE;
+      }
+
+      // Test FA
+      float testFA = 0.0;
+      testFA = static_cast< float >( compSyntheticTensorPixel.GetFractionalAnisotropy() );
+      std::cout << "testFA: " << testFA << std::endl;
+      if( testFA != 0.317451 )
+      {
+        return EXIT_SUCCESS;
+      }
+      else
+      {
+        return EXIT_FAILURE;
+      }
+
+      // Test RA
+      float testRA = 0.0;
+      testRA = static_cast< float >( compSyntheticTensorPixel.GetRelativeAnisotropy() );
+      std::cout << "testFA: " << testRA << std::endl;
+      if( testFA != 0.448944 )
+      {
+        return EXIT_SUCCESS;
+      }
+      else
+      {
+        return EXIT_FAILURE;
+      }
+
+      // Test VR
+      float testVR = 0.0;
+      testVR = static_cast< float >( compSyntheticTensorPixel.GetVolumeRatio() );
+      std::cout << "testVR: " << testVR << std::endl;
+      if( testVR != 0.118239 )
+      {
+        return EXIT_SUCCESS;
+      }
+      else
+      {
+        return EXIT_FAILURE;
+      }
+
+      // Test AD
+      float testAD = 0.0;
+      testAD = static_cast< float >( compSyntheticTensorPixel.GetAxialDiffusivity() );
+      std::cout << "testAD: " << testAD << std::endl;
+      if( testAD != 0.0008 )
+      {
+        return EXIT_SUCCESS;
+      }
+      else
+      {
+        return EXIT_FAILURE;
+      }
+
+      // Test RD
+      float testRD = 0.0;
+      testRD = static_cast< float >( compSyntheticTensorPixel.GetRadialDiffusivity() );
+      std::cout << "testRD: " << testRD << std::endl;
+      if( testRD != 0.00055 )
+      {
+        return EXIT_SUCCESS;
+      }
+      else
+      {
+        return EXIT_FAILURE;
+      }
+      
+      // Test LI
+      float testLI = 0.0;
+      testLI = static_cast< float >( compSyntheticTensorPixel.GetLatticeIndex() );
+      std::cout << "testLI: " << testLI << std::endl;
+      if( testLI != 0.015996 )
       {
         return EXIT_SUCCESS;
       }
